f730d0ad043b92ce9fdfa16941a945d5437defc0
Rishith <rishithrishith1@gmail.com>
2024-11-22 22:02:29 +0530

    new files

--- UNIT-2/practice.cpp
+++ UNIT-2/practice.cpp
@@ -104,20 +104,90 @@ int main(){
 
 
 // Modular Multiplicative Inverse
-long long modExp(long long a,long long b,long long p){
+/*long long modExp(long long a,long long b,long long p){
     long long res=1;
     a=a%p;
     if(a==0) return 0;
     while(b>0){
-        if()
+        if(b&1){
+            res=(res*a)%p;
+        }
+        b=b>>1;
+        a=(a*a)%p;
     }
+    return res;
 }
 long long invMul(long long a,long long m){
-    return modExp(a,m-2,m);
+    return modExp(a,m-2,m); //a^(m-2)%m
 }
 
 int main(){
     long long a,m;
     cin>>a>>m;
     cout<<invMul(a,m);
+}
+*/
+
+// Sum of Middle Row and Element in Matrix
+/*int sumMiddle(vector<vector<int>> &arr, int n){
+    int rows=arr.size();
+    int cols=arr[0].size();
+    int middlerow=rows/2;
+    int sum=0;
+    for(int i=0;i<cols;i++){
+        sum+=arr[middlerow][i];
+    }
+    return sum;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>> arr(n,vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<sumMiddle(arr,n);
+}
+*/
+
+// Checking if All Rows of a Matrix are Circular Rotations of Each Other
+bool iscircular(const vector<int> &row1,const vector<int> &row2){
+    if(row1.size()!=row2.size()) return false;
+    string s1="";
+    string s2="";
+    for(int num:row1) s1+=to_string(num)+",";
+    for(int num:row2) s2+=to_string(num)+",";
+    s1+=s1;
+    return s1.find(s2)!=string::npos;
+}
+
+bool arecircular(vector<vector<int>> &arr){
+    int n=arr.size();
+    for(int i=1;i<n;i++){
+        if(!iscircular(arr[i-1],arr[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>> arr(n,vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    if(arecircular(arr)){
+        cout<<"it is a circular matrix";
+    }
+    else{
+        cout<<"it is non circular matrix";
+    }
 }
