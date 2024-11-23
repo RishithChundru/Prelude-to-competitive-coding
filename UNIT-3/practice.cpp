4983222448fbf876e823dec874a835ee7537adc2
Rishith <rishithrishith1@gmail.com>
2024-11-23 16:13:15 +0530

    new files

--- UNIT-3/practice.cpp
+++ UNIT-3/practice.cpp
@@ -39,18 +39,27 @@ int main(){
 
 // Total Number of Divisors of a Number
 
-int totaldivisors(int n){
+// int totaldivisors(int n){
+//     int count=0;
+//     int divisors=1;
+//     for(int i=2;i*i<n;i++){
+//         while(n%i==0){
+//             n=n/i;
+//             count++;
+//         }
+//         divisors*=(count+1);
+//     }
+//     if(n>1) divisors*=2;
+//     return divisors;
+// }
+/*int totaldivisors(int n){
     int count=0;
-    int divisors=1;
-    for(int i=2;i*i<n;i++){
-        while(n%i==0){
-            n=n/i;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
             count++;
         }
-        divisors*=(count+1);
     }
-    if(n>1) divisors*=2;
-    return divisors;
+    return count;
 }
 
 int main(){
@@ -58,4 +67,107 @@ int main(){
     cin>>n;
     int res=totaldivisors(n);
     cout<<res<<endl;
+}
+*/
+
+
+//Finding All Prime Factors of a Number
+/*void primefactors(int n){
+    vector<int> arr;
+    for(int i=2;i*i<=n;i++){
+        while(n%i==0){
+            arr.push_back(i);
+            n=n/i;
+        }
+    }
+    if(n>1) arr.push_back(n);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    primefactors(n);
+}
+*/
+
+
+//k-jagged number
+
+/*bool kjagged(int n,int k){
+    int kmax=2;
+    for(int i=2;i*i<=n;i++){
+        while(n%i==0){
+            n=n/i;
+            kmax=i;
+        }
+    }
+    if(n>1) kmax=n;
+    return kmax<=k;
+}
+int main(){
+    int n;
+    int k;
+    cin>>n;
+    cin>>k;
+    if(kjagged(n,k)) cout<<"it is k-jagged number";
+    else cout<<"it is not jagged number";
+}
+*/
+
+// Stormer number
+/*int kjagged(int n){
+    int kmax=2;
+    for(int i=2;i*i<=n;i++){
+        while(n%i==0){
+            kmax=i;
+            n=n/i;
+        }
+    }
+    if(n>1) kmax=n;
+    return kmax;
+}
+bool isstormer(int n){
+    int maxprimefactor=kjagged(n*n+1);
+    return maxprimefactor>=2*n;
+}
+int main(){
+    int k;
+    cin >> k;
+    cout<<(isstormer(k)?"yes":"no")<<endl;
+}*/
+
+
+// finding prime factors by taking square root
+/*void primefactor(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            cout<<i<<" ";
+            n=n/i;
+        }
+    }
+    if(n>1) cout<<n<<" ";
+}
+int main(){
+    int n;
+    cin>>n;
+    primefactor(n);
+}
+*/
+
+// p-smooth numbers in given ranges
+
+vector<int> findprime(int start,int end,int p){
+    vector<int> factors;
+    for(int i=start;i<=end;i++){
+        
+    }
+}
+int main(){
+    int start,end,p;
+    cin>>start>>end>>p;
+    vector<int> factors=findprime(start,end,p);
+    for(int i:factors) cout<<i<<" ";
 }
